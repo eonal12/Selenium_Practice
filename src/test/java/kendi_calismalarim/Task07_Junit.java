dc36924540b018951ea426f73661f1aa540ddf27
Emre QA <emreonal12@gmail.com>
2023-04-12 07:54:04 +0300

    ...

---
+++ src/test/java/kendi_calismalarim/Task07_Junit.java
@@ -0,0 +1,38 @@
+package kendi_calismalarim;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class Task07_Junit extends TestBase {
+
+    //- http://szimek.github.io/signature_pad/ sayfasına gidiniz
+    //- Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
+    //- Çizimden sonra clear butonuna basınız
+    //- Sayfayi kapatiniz
+
+
+    @Test
+    public void name() {
+        driver.get("http://szimek.github.io/signature_pad/");
+        WebElement yaziTahtasi = driver.findElement(By.xpath("//canvas"));
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(yaziTahtasi);
+
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(-5,-5);
+        }
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(0,5);
+        }
+        for (int i = 0; i < 10; i++) {
+            actions.moveByOffset(5,0);
+        }
+        actions.release().perform();
+
+        bekle(5);
+        driver.findElement(By.xpath("//*[text()='Clear']")).click();
+    }
+}
